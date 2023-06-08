255e72257a0dda3abf8c6209e59a249470073a22
CAB233 <2385771849@qq.com>
2023-06-08 16:50:01 +0800

    修改端口，增加根目录变量

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine:latest
 ENV REFRESH_TOKEN \
     WEBDAV_AUTH_USER \
     WEBDAV_AUTH_PASSWORD \
-    WORKPATH
+    ROOT
 
 WORKDIR /app
 
@@ -13,5 +13,5 @@ COPY run.sh /app/
 RUN chmod +x aliyundrive-webdav && \
     chmod +x run.sh
 
-EXPOSE 3080
+EXPOSE 443
 CMD /app/run.sh
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/app/aliyundrive-webdav --port 3080 --refresh-token $REFRESH_TOKEN --auth-user $WEBDAV_AUTH_USER --auth-password $WEBDAV_AUTH_PASSWORD --workdir $WORKPATH --read-only
+/app/aliyundrive-webdav --port 443 --refresh-token $REFRESH_TOKEN --auth-user $WEBDAV_AUTH_USER --auth-password $WEBDAV_AUTH_PASSWORD --root $ROOT --read-only --auto-index
